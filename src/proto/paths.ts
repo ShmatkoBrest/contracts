5b5e4317246540304c7404560bb3b55dabdc0007
Максим <ustimchukmaxim@gmail.com>
2026-02-09 15:35:09 +0300

    feat: add account service proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -1,5 +1,7 @@
 import { join } from "path";
 
 export const PROTO_PATHS = {
-    AUTH: join(__dirname, '../../proto/auth.proto')
+    AUTH: join(__dirname, '../../proto/auth.proto'),
+    ACCOUNT: join(__dirname, '../../proto/account.proto'),
+
 } as const
