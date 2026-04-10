a5d2f18cc3b8aa883dc61d9bbb9f426998b73105
Максим <ustimchukmaxim@gmail.com>
2026-04-10 09:47:32 +0300

    feat: add media.proto fix

--- scripts/generate_go.sh
+++ scripts/generate_go.sh
@@ -6,8 +6,8 @@ echo "Generating Go protobuf (media)..."
 protoc -I ./proto \
     --go_out=./gen/go \
     --go-grpc_out=./gen/go \
-    --go_opt=module=github.com/ShamtkoBrest/contracts \
-    --go-grpc_opt=module=github.com/ShamtkoBrest/contracts \
+    --go_opt=module=github.com/ShmatkoBrest/contracts \
+    --go-grpc_opt=module=github.com/ShmatkoBrest/contracts \
     ./proto/media.proto
 
 echo "Go generation complete."
