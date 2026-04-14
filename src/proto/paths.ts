0e01473a4ff85f441ad7afb0ce5431cba039c059
Максим <ustimchukmaxim@gmail.com>
2026-04-14 16:04:53 +0300

    fix: add media to path

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -3,5 +3,6 @@ import { join } from "path";
 export const PROTO_PATHS = {
     AUTH: join(__dirname, '../../proto/auth.proto'),
     ACCOUNT: join(__dirname, '../../proto/account.proto'),
-    USERS: join(__dirname, '../../proto/users.proto')
+    USERS: join(__dirname, '../../proto/users.proto'),
+    MEDIA: join(__dirname, '../../proto/media.proto')
 } as const
